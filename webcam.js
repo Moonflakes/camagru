fa52148287e8c4e36a9dbbb56ca009e9b0b47583
Camila Masetti <cmasetti@student.42.fr>
2018-10-29 15:47:34 +0100

    plusieur s objets + css

--- webcam.js
+++ webcam.js
@@ -18,15 +18,46 @@
   var canvas = null;
   var photo = null;
   var startbutton = null;
+
   var glasses = null;
+  var chapka = null;
+  var canard = null;
+  var couronne = null;
+  var suit = null;
+  var chain = null;
+
+  var checkGlasses = null;
+  var checkChain = null;
+  var checkCouronne = null;
+  var checkSuit = null;
+  var checkCanard = null;
+  var checkChapka = null;
 
   function startup() {
     video = document.getElementById('video');
     canvas = document.getElementById('canvas');
     photo = document.getElementById('photo');
     startbutton = document.getElementById('startbutton');
+
     glasses = document.getElementById('glasses');
     glasses.setAttribute('crossOrigin', 'anonymous');
+    chapka = document.getElementById('chapka');
+    chapka.setAttribute('crossOrigin', 'anonymous');
+    chain = document.getElementById('chain');
+    chain.setAttribute('crossOrigin', 'anonymous');
+    canard = document.getElementById('canard');
+    canard.setAttribute('crossOrigin', 'anonymous');
+    suit = document.getElementById('suit');
+    suit.setAttribute('crossOrigin', 'anonymous');
+    couronne = document.getElementById('couronne');
+    couronne.setAttribute('crossOrigin', 'anonymous');
+
+    checkGlasses = document.getElementById("OKglasses");
+    checkChain = document.getElementById("OKchain");
+    checkCouronne = document.getElementById("OKcouronne");
+    checkSuit = document.getElementById("OKsuit");
+    checkCanard = document.getElementById("OKcanard");
+    checkChapka = document.getElementById("OKchapka");
 
     // Older browsers might not implement mediaDevices at all, so we set an empty object first
 		if (navigator.mediaDevices === undefined) {
@@ -69,23 +100,22 @@
 		    video.play();
 		  };
 		})
-		.catch(function(err) {
+	/*	.catch(function(err) {
 		  console.log(err.name + ": " + err.message);
 		});
-
-		//
+  */
 
     video.addEventListener('canplay', function(ev){
       if (!streaming) {
         height = video.videoHeight / (video.videoWidth/width);
-      
+
         // Firefox currently has a bug where the height can't be read from
         // the video, so we will make assumptions if this happens.
-      
+
         if (isNaN(height)) {
           height = width / (4/3);
         }
-      
+
         video.setAttribute('width', width);
         video.setAttribute('height', height);
         canvas.setAttribute('width', width);
@@ -98,7 +128,7 @@
       takepicture();
       ev.preventDefault();
     }, false);
-    
+
     clearphoto();
   }
 
@@ -107,13 +137,13 @@
 
   function clearphoto() {
     var context = canvas.getContext('2d');
-    context.fillStyle = "#AAA";
+    context.fillStyle = "aquamarine";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     var data = canvas.toDataURL('image/png');
     photo.setAttribute('src', data);
   }
-  
+
   // Capture a photo by fetching the current contents of the video
   // and drawing it into a canvas, then converting that to a PNG
   // format data URL. By drawing it on an offscreen canvas and then
@@ -126,8 +156,24 @@
       canvas.width = width;
       canvas.height = height;
       context.drawImage(video, 0, 0, width, height);
-      context.drawImage(glasses, 100, 100, 70, 50);
-    
+      if (checkGlasses.checked == true){
+        context.drawImage(glasses, 125, 100, 70, 15);
+      }
+      if (checkChapka.checked == true){
+        context.drawImage(chapka, 110, 30, 100, 60);
+      }
+      if (checkChain.checked == true){
+        context.drawImage(chain, 120, 150, 90, 50);
+      }
+      if (checkCanard.checked == true){
+        context.drawImage(canard, 0, 180, 60, 60);
+      }
+      if (checkCouronne.checked == true){
+        context.drawImage(couronne, 110, 20, 100, 60);
+      }
+      if (checkSuit.checked == true){
+        context.drawImage(suit, 40, 160, 250, 80);
+      }
       var data = canvas.toDataURL('image/png');
       photo.setAttribute('src', data);
     } else {
