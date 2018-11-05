1c4c1d12a3b224fd0336a8fd2d50934424411e5e
thiery margot <margot.thiery@gmail.com>
2018-11-05 01:10:53 +0100

    essai css

---
+++ essai.php
@@ -0,0 +1,61 @@
+<html>
+    <head>
+        <title>Camagru</title>
+        <link rel="stylesheet" type="text/css" href="essai.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <div class="lien_logo">
+            <a href="index.php" title="Accueil">
+                <img class="logo" src="img_site/logo.png" alt="home" title="Home">
+            </a>
+        </div>
+        <div>
+        <ul class="icones">
+            <li>
+                <a href="index.php" title="Home">
+                    <img src="img_site/icones/camera_1.png" alt="home" title="Home">
+                </a>
+            </li>
+            <li>
+                <a href="index.php" title="Home">
+                    <img src="img_site/icones/home_1.png" alt="home" title="Home">
+                </a>
+            </li>
+            <li>
+                <a href="index.php" title="Paramètres">
+                    <img src="img_site/icones/settings_green.png" alt="home" title="Home">
+                </a>
+            </li>
+            <li>
+                <a href="index.php" title="Login">
+                    <img src="img_site/icones/login_1.png" alt="home" title="Home">
+                </a>
+            </li>
+            <li>
+                <a href="index.php" title="Sign Up">
+                    <img src="img_site/icones/signup.png" alt="home" title="Home">
+                </a>
+            </li>
+        </ul>
+        <ul class="icones">
+            <li>
+                <span>Prendre une photo</span>
+            </li>
+            <li>
+                <span>Accueil</span>
+            </li>
+            <li>
+                <span>Paramètres</span>
+            </li>
+            <li>
+                <span>Login</span>
+            </li>
+            <li>
+                <span>Signup</span>
+            </li>
+        </ul>
+        </div>
+    </body>
+</html>
