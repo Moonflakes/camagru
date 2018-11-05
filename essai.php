a30d8dbc1956658fd6a21a7a5f12a681edd0aa2a
Moonflakes <mthiery@student.42.fr>
2018-11-05 16:09:21 +0100

    essai animation icones

--- essai.php
+++ essai.php
@@ -12,34 +12,29 @@
             </a>
         </div>
         <div>
-        <ul class="icones">
-            <li>
+        <div class="icones">
                 <a href="index.php" title="Home">
                     <img src="img_site/icones/camera_1.png" alt="home" title="Home">
+                    <h3>Prendre une photo</h3>
                 </a>
-            </li>
-            <li>
                 <a href="index.php" title="Home">
                     <img src="img_site/icones/home_1.png" alt="home" title="Home">
+                    <h3>Home</h3>
                 </a>
-            </li>
-            <li>
                 <a href="index.php" title="Paramètres">
                     <img src="img_site/icones/settings_green.png" alt="home" title="Home">
+                    <h3>Paramètres</h3>
                 </a>
-            </li>
-            <li>
                 <a href="index.php" title="Login">
                     <img src="img_site/icones/login_1.png" alt="home" title="Home">
+                    <h3>Login</h3>
                 </a>
-            </li>
-            <li>
                 <a href="index.php" title="Sign Up">
                     <img src="img_site/icones/signup.png" alt="home" title="Home">
+                    <h3>Sign Up</h3>
                 </a>
-            </li>
-        </ul>
-        <ul class="icones">
+        </div>
+    <!--    <ul class="icones">
             <li>
                 <span>Prendre une photo</span>
             </li>
@@ -56,6 +51,17 @@
                 <span>Signup</span>
             </li>
         </ul>
-        </div>
+        </div>  -->
     </body>
+    <!--<script type="text/javascript">
+        $(document).ready(function () 
+        {
+            $('h3').css({height:0, margin:0});
+
+            $('a').bind("mouseover", function()
+            {
+                $('h3',this).animate({"heigth":"40px"},300);
+            })
+        })
+    </script> -->
 </html>
---
+++ essai2.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Camagru</title>
+        <link rel="stylesheet" type="text/css" href="essai2.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="div1">
+                <a href="index.php" title="Home">
+                    <img src="img_site/icones/camera_1.png" alt="home" title="Home">
+                </a>
+                <a href="index.php" title="Home">
+                    <img src="img_site/icones/home_1.png" alt="home" title="Home">
+                </a>
+                <a href="index.php" title="Paramètres">
+                    <img src="img_site/icones/settings_green.png" alt="home" title="Home">
+                </a>
+            </div>
+            <div class="div2">
+                <a href="index.php" title="Accueil">
+                    <img class="logo" src="img_site/logo.png" alt="home" title="Home">
+                </a>
+            </div>
+            <div class="div3">
+                <a href="index.php" title="Login">
+                    <img src="img_site/icones/login_1.png" alt="home" title="Home">
+                </a>
+                <a href="index.php" title="Sign Up">
+                    <img src="img_site/icones/signup.png" alt="home" title="Home">
+                </a>
+            </div>
+        </div>
+    </body>
+</html>
