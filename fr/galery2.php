b88a5dd0882a35fe9cdc8b37c18652557729fa03
thiery margot <margot.thiery@gmail.com>
2018-11-18 20:35:58 +0100

    masonry js en cours

--- fr/galery2.php
+++ fr/galery2.php
@@ -10,7 +10,7 @@
             <option value="40" <?php if (isset($_GET['limit']) && $_GET['limit'] == 40) echo "selected"?>>40</option>
         </select>
     </form>
-    <div class="grid">
+    <div class="grid" id="grid">
 <?php
     include_once '../config/setup.php';
     include_once '../include/paginator.php';
@@ -27,6 +27,7 @@
     //print_r($result->data);
     if(isset($result->data))
     {
+        $i = 0;
         foreach ($result->data as $key => $array) 
         {
             $path = 0;
@@ -55,8 +56,9 @@
             }
             if ($path)
             {
+                $i = $i++;
 ?>
-        <div class="item_photo">
+        <div class="item_photo" id="item<?php echo $i++;?>">
             <div class="content_item">
             <!--    <img class="pince" src="../img_site/pince.png" alt="pince"> -->
                 <figure>
@@ -97,7 +99,169 @@
 <?php
     echo $paginator->createLinks($links, 'pagination');
 ?>
-<script>
+<script type="text/javascript">
+function organisation(nbcol)
+{
+    var column1 = document.getElementById('column1');
+    var column2 = document.getElementById('column2');
+    var column3 = document.getElementById('column3');
+    var column4 = document.getElementById('column4');
+    var item;
+    var i = 0;
+    if (nbcol === 1)
+    {
+        //mettre les éléments dans la div
+        while (item = document.getElementById('item'+(i++)))
+            column1.appendChild(item);
+        //list.removeChild(list.childNodes[0]);
+    }
+    else if (nbcol === 2)
+    {
+        while (item = document.getElementById('item'+(i++)))
+        {
+            if (i % 2 == 0)
+                column2.appendChild(item);
+            else
+                column1.appendChild(item);
+        }
+    }
+    else if (nbcol === 3)
+    {
+        while (item = document.getElementById('item'+(i++)))
+        {
+            if (i % 3 == 0)
+                column3.appendChild(item);
+            else if (((i - 1) != 0 && (i - 1) % 3 == 0) || i == 1)
+                column1.appendChild(item);
+            else if (((i + 1) != 0 && (i + 1) % 3 == 0) || i == 2)
+                column2.appendChild(item);
+        }
+    }
+    else if (nbcol === 4)
+    {
+        while (item = document.getElementById('item'+(i++)))
+        {
+            if (i % 4 == 0)
+                column4.appendChild(item);
+            else if (((i - 1) != 0 && (i - 1) % 4 == 0) || i == 1)
+                column1.appendChild(item);
+            else if (((i - 2) != 0 && (i - 2) % 4 == 0) || i == 2)
+                column2.appendChild(item);
+            else if (((i - 3) != 0 && (i - 3) % 4 == 0) || i == 3)
+                column3.appendChild(item);
+        }
+    }
+}
+
+function createColumns(largeur)
+{
+    var grid = document.getElementById("grid");
+    var column1 = document.getElementById('column1');
+    var column2 = document.getElementById('column2');
+    var column3 = document.getElementById('column3');
+    var column4 = document.getElementById('column4');
+    var nbcol = 0;
+
+    if (column1 == undefined)
+    {
+        column1 = document.createElement('div');
+        column1.setAttribute("id", "column1");
+        column1.setAttribute("class", "column");
+        grid.appendChild(column1);
+        nbcol = 1;
+    }
+    if (column2 == undefined)
+    {
+        if (largeur > 700)
+        {
+            //create column
+            column2 = document.createElement('div');
+            column2.setAttribute("id", "column2");
+            column2.setAttribute("class", "column");
+            grid.appendChild(column2);
+            nbcol = 2;
+        }
+    }
+    else
+    {
+        if (largeur < 700)
+        {
+            // reorganisation 
+            organisation(1);
+            
+            //remove column
+            grid.removeChild(column2);
+            return;
+        }
+    }
+    if (column3 == undefined)
+    {
+        if (largeur > 1100)
+        {
+            column3 = document.createElement('div');
+            column3.setAttribute("id", "column3");
+            column3.setAttribute("class", "column");
+            grid.appendChild(column3);
+            nbcol = 3;
+        }
+    }
+    else
+    {
+        if (largeur < 1100)
+        {
+            // reorganisation 
+            organisation(2);
+
+            // remove column
+            grid.removeChild(column3);
+            return;
+        }
+    }
+    if (column4 == undefined)
+    {
+        if (document.body.clientWidth > 1300)
+        {
+            column4 = document.createElement('div');
+            column4.setAttribute("id", "column4");
+            column4.setAttribute("class", "column");
+            grid.appendChild(column4);
+            nbcol = 4;
+        }
+    }
+    else
+    {
+        if (document.body.clientWidth < 1300)
+        {
+            // reorganisation 
+            organisation(3);
+
+            // remove column
+            grid.removeChild(column4);
+            return;
+        }
+    }
+    organisation(nbcol);
+}
+
+
+window.addEventListener('load',function(){
+    var largeur = document.body.clientWidth;
+    createColumns(largeur);
+
+//controler la largeur de la fenetre
+    this.addEventListener('resize',function(){
+        largeur = document.body.clientWidth;
+        createColumns(largeur);
+    });
+});
+
+//mettre les éléments dans la div
+//document.getElementById('div3').appendChild(document.getElementById('div1'))
+//document.getElementById('div3').appendChild(document.getElementById('div2'))
+//console.log(document.getElementById('div3').innerHTML)
+//list.removeChild(list.childNodes[0]);
+	</script>
+<!-- <script>
     function resizeGridItem(item){
     grid = document.getElementsByClassName("grid")[0];
     rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
@@ -125,5 +289,5 @@
   for(x=0;x<allItems.length;x++){
     imagesLoaded( allItems[x], resizeInstance);
   }
-    </script>
+    </script> -->
 </section>
