868d1556f3a347fc32e00047695d3274f9458383
Moonflakes <mthiery@student.42.fr>
2018-12-14 17:20:36 +0100

    merge all filter ok

--- include/take_photo.php
+++ include/take_photo.php
@@ -15,19 +15,21 @@ if (isset($_POST['submit']))
     $img = str_replace(' ', '+', $camera[1]);
     $data = base64_decode($img);
     
-    $im = imagecreatefromstring($data);
-    $im2 = imagecreatefrompng($src[0]);
-    $size = getimagesize($src[0]);
+    $im = imagecreatefromstring($data); //créer l'image de la camera
 
-    if (imagecopyresized($im, $im2, $left[0], $top[0], 0, 0, $width[0], $height[0], $size[0], $size[1]))
-    {
-        $o = "lala";
-        ob_start(); // Let's start output buffering.
-            imagepng($im); //This will normally output the image, but because of ob_start(), it won't.
-            $contents = ob_get_contents(); //Instead, output above is saved to $contents
-        ob_end_clean(); //End the output buffer.
-        $encode = base64_encode($contents);
-        $str = $camera[0].",".$encode;
+    foreach ($name as $key => $value) {
+        $im2 = imagecreatefrompng($src[$key]);
+        $size = getimagesize($src[$key]);
+
+        if (imagecopyresized($im, $im2, $left[$key], $top[$key], 0, 0, $width[$key], $height[$key], $size[0], $size[1]))
+        {
+            ob_start(); // Let's start output buffering.
+                imagepng($im); //This will normally output the image, but because of ob_start(), it won't.
+                $contents = ob_get_contents(); //Instead, output above is saved to $contents
+            ob_end_clean(); //End the output buffer.
+            $encode = base64_encode($contents);
+            $str = $camera[0].",".$encode;
+        }
     }
     $arr = array("submit" => $_POST['submit'], "cam" => $u, "data" => $str, "o" => $size);
 }
