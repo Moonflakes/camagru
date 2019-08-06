785e1fb8982ef588dea2113bc8b035d439e73b02
Moonflakes <mthiery@student.42.fr>
2019-08-06 16:37:53 +0200

    delete  files

--- logout2.php
+++
@@ -1,7 +0,0 @@
-<form action="include/logout.inc.php" method="POST">
-    <button type="submit" name="submit"><img src="img_site/icones/logout_3.png" alt="logout" title="Logout"
-        style="width:50; margin-right: 20px; cursor: pointer;"></button>
-</form>
-<a href="account.php" id="signup-link">
-    <img src="img_site/icones/account_green.png" alt="account" title="Mon compte"
-        style="width:50; margin-right: 20px; cursor: pointer;"></a>
